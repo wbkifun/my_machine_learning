745bee4f5b34e92827ca8b30db311ecb48894741
Ki-Hwan Kim <wbkifun@nate.com>
2019-04-12 15:28:06 +0900

    NAND, OR, XOR
    multi-layer perceptron

--- deep_learning_from_scratch/ch2_perceptron/perceptron.py
+++ deep_learning_from_scratch/ch2_perceptron/perceptron.py
@@ -11,22 +11,37 @@ Created on Wed Apr 10 22:33:31 2019
 
 
 
-def percep(x1, x2, w1, w2, theta):
-    val = x1*w1 + x2*w2
+def percep(x1, x2, w1, w2, bias):
+    val = x1*w1 + x2*w2 + bias
     
-    if val <= theta:
-        return 0
-    else:
-        return 1
+    return 0 if val <= 0 else 1
     
     
     
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
+    w1, w2, theta = 0.5, 0.5, -0.7
     return percep(x1, x2, w1, w2, theta)
     
 
 
 def NAND(x1, x2):
-    w1, w2, theta = -0.5, -0.5, -0.7
+    w1, w2, theta = -0.5, -0.5, 0.7
     return percep(x1, x2, w1, w2, theta)
+
+
+
+def OR(x1, x2):
+    w1, w2, theta = 0.5, 0.5, -0.4
+    return percep(x1, x2, w1, w2, theta)
+
+
+
+def XOR(x1, x2):
+    '''
+    Multi-layer perceptron
+    '''
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    out = AND(s1, s2)
+    
+    return out
--- deep_learning_from_scratch/ch2_perceptron/test_perceptron.py
+++ deep_learning_from_scratch/ch2_perceptron/test_perceptron.py
@@ -6,7 +6,8 @@ Created on Wed Apr 10 22:35:36 2019
 """
 
 from numpy.testing import assert_equal as equal
-from perceptron import AND, NAND
+from perceptron import AND, NAND, OR, XOR
+
 
 
 def test_AND():
@@ -16,8 +17,25 @@ def test_AND():
     equal(AND(1, 1), 1)
     
     
+    
 def test_NAND():
     equal(NAND(0, 0), 1)
     equal(NAND(1, 0), 1)
     equal(NAND(0, 1), 1)
     equal(NAND(1, 1), 0)
+    
+  
+    
+def test_OR():
+    equal(OR(0, 0), 0)
+    equal(OR(1, 0), 1)
+    equal(OR(0, 1), 1)
+    equal(OR(1, 1), 1)
+    
+ 
+    
+def test_XOR():
+    equal(XOR(0, 0), 0)
+    equal(XOR(1, 0), 1)
+    equal(XOR(0, 1), 1)
+    equal(XOR(1, 1), 0)
